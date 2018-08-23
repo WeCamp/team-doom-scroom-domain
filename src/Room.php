720712e94905c011a7f538b86129747ff456cc59
Glenn Letteboer <glenn.letteboer@takeaway.local>
2018-08-23 09:14:05 +0200

    Implement: room enters a room

--- src/Room.php
+++ src/Room.php
@@ -9,20 +9,44 @@ final class Room
      */
     private $name;
 
+    /**
+     * @var Loon[]
+     */
+    private $loons;
+
     public static function openUp(string $name): self
     {
-        if (!trim($name)) {
-            throw new \InvalidArgumentException('Room-name cannot be empty');
-        }
-
-        $room = new self();
-        $room->name = $name;
-
-        return $room;
+        return new self($name);
     }
 
     public function name(): string
     {
         return $this->name;
+    }
+
+    public function receiveLoon(Loon $loon): void
+    {
+        $this->loons[] = $loon;
+    }
+
+    public function hasReceived(Loon $loon): bool
+    {
+        foreach ($this->loons as $loonInRoom) {
+            if ($loon->id() === $loonInRoom->id()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private function __construct(string $name)
+    {
+        if (!trim($name)) {
+            throw new \InvalidArgumentException('Room-name cannot be empty');
+        }
+
+        $this->name = $name;
+        $this->loons = [];
     }
 }
---
+++ tests/LoonTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Scroom\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Scroom\Loon;
+use Scroom\Room;
+
+final class LoonTest extends TestCase
+{
+    /**
+     * @var Room;
+     */
+    private $room;
+
+    protected function setUp(): void
+    {
+        $this->room = Room::openUp('Some name');
+    }
+
+    protected function tearDown(): void
+    {
+        $this->room = null;
+    }
+
+    /**
+     * @test
+     */
+    public function itEntersARoom(): void
+    {
+        $loon = Loon::enter($this->room);
+
+        $this->assertInstanceOf(Loon::class, $loon);
+        $this->assertTrue($this->room->hasReceived($loon));
+    }
+}
